8cffc45a079d8992020635c5df1a4e6480c6bf73
francisco-edu <mesquita.franciscoedu@gmail.com>
2025-11-08 18:14:27 -0300

    codigo q implementa o onibus

---
+++ backend/src/main/java/com/campimove/backend/dto/OnibusDTO.java
@@ -0,0 +1,12 @@
+package com.campimove.backend.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class OnibusDTO {
+    private String placa;
+    private String modelo;
+    private int capacidade;
+}
---
+++ backend/src/main/java/com/campimove/backend/repository/OnibusRepository.java
@@ -0,0 +1,7 @@
+package com.campimove.backend.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.campimove.backend.entity.Onibus;
+
+public interface OnibusRepository extends JpaRepository<Onibus, Long> {
+}
