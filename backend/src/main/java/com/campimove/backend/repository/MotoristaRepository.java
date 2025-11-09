18e3d374de11525e2951a9f0d8414999be9a5d8a
francisco-edu <mesquita.franciscoedu@gmail.com>
2025-11-09 01:16:51 -0300

    Delete backend/src/main/java/com/campimove/backend/repository/MotoristaRepository.java
    codigo fora do caso de uso

--- backend/src/main/java/com/campimove/backend/repository/MotoristaRepository.java
+++
@@ -1,7 +0,0 @@
-package com.campimove.backend.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.campimove.backend.entity.Motorista;
-
-public interface MotoristaRepository extends JpaRepository<Motorista, Long> {
-}
