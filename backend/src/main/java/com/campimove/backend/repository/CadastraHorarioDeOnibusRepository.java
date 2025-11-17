4c1733078a62657cd0091747ddae3f8a0af4b255
francisco-edu <mesquita.franciscoedu@gmail.com>
2025-11-16 21:23:18 -0300

    Refactor repository to include IntercampiRouteRepository
    repository certo

--- backend/src/main/java/com/campimove/backend/repository/CadastraHorarioDeOnibusRepository.java
+++ backend/src/main/java/com/campimove/backend/repository/CadastraHorarioDeOnibusRepository.java
@@ -1,7 +1,6 @@
 package com.campimove.backend.repository;
 
-import com.campimove.backend.entity.CadastraHorarioDeOnibus;
+import com.campimove.backend.entity.IntercampiRoute;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface CadastraHorarioDeOnibusRepository extends JpaRepository<CadastraHorarioDeOnibus, Long> {
-}
+public interface IntercampiRouteRepository extends JpaRepository<IntercampiRoute, Long> {}
