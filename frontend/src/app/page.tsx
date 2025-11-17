76db8d92334ac08a2fc6afe710562cd0331aa626
pedropsaraiva <pedropsaraiva21@gmail.com>
2025-11-17 10:41:30 -0300

    redirecionamento da pagina inicial para o dashboard usando token

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -4,8 +4,23 @@ import { Features } from '@/components/landing/features';
 import { Testimonials } from '@/components/landing/testimonials';
 import { Cta } from '@/components/landing/cta';
 import { Footer } from '@/components/landing/footer';
+import { useRouter } from "next/navigation";
 
 export default function Home() {
+    const router = useRouter();
+
+    useEffect(() => {
+            if (typeof window === 'undefined') return;
+
+            const token = localStorage.getItem('jwt_token');
+            const userRole = localStorage.getItem('user_role');
+
+            if (!token) return router.push("/");
+            if (data.role == "ADMIN") return router.push("/dashboard/admin")
+            if (data.role == "DRIVER") return router.push("/dashboard/motorist")
+            return router.replace('/dashboard');
+            }, [router]);
+
   return (
     <div className="flex flex-col min-h-dvh bg-background">
       <Header />
